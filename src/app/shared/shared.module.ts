eebb135858c618ff125d44643f0f0efc542c6ae9
ygout <yohann.goutaret@gmail.com>
2020-02-19 20:19:35 +0100

    add template ngx boostrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxBootstrapModule
   ],
   exports: [
     CommonModule
