e8d09b48ae1dc798b70a01b330344a32e81956c4
ygout <yohann.goutaret@gmail.com>
2020-02-24 15:19:07 +0100

    router public + navbar

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { SharedPublicModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
-    CommonModule
+    SharedPublicModule
   ]
 })
 export class HomeModule { }
