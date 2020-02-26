c41e532ceb5bb37c06c187ce9168c389953de763
ygout <yohann.goutaret@gmail.com>
2020-02-26 22:58:26 +0100

    add nav bar html + css

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
 import { SharedPublicModule } from '../public/shared/shared.module';
 
 
@@ -9,12 +8,10 @@ import { SharedPublicModule } from '../public/shared/shared.module';
   declarations: [],
   imports: [
     CommonModule,
-    NgxBootstrapModule,
     SharedPublicModule
   ],
   exports: [
     CommonModule,
-    NgxBootstrapModule,
     SharedPublicModule
   ]
 })
