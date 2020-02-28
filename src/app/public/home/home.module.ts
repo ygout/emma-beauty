42aaba546c01b8b31931c607fb6b76736ad13edd
ygout <yohann.goutaret@gmail.com>
2020-02-29 00:16:54 +0100

    navbar fixed top + sticky top with shrink + add banner HomeComponent

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedPublicModule } from '../shared/shared.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeBannerComponent],
   imports: [
     SharedModule
   ]
--- src/app/public/shared/components/navbar/navbar.component.ts
+++ src/app/public/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+
+export class NavbarComponent implements OnInit, AfterViewInit{
+
+  @ViewChild('stickyMenu') menuElement: ElementRef;
+
+  sticky = false;
+  elementPosition: number;
+
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event) {
+    const windowScroll: number = window.pageYOffset;
+    if (windowScroll > this.elementPosition)
+    {
+      this.sticky = true;
+
+    } else {
+      this.sticky = false;
+    }
   }
 
 }
