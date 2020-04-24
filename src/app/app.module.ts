d5327170396a543e7a2b73a707d4f6ec39e93f83
ygout <yohann.goutaret@gmail.com>
2020-04-24 21:50:21 +0200

    in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { SharedPublicModule } from './public/shared/shared.module';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { SharedPublicModule } from './public/shared/shared.module';
     CoreModule,
     BrowserAnimationsModule,
     SharedModule,
-    SharedPublicModule
+    SharedPublicModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [Title],
   bootstrap: [AppComponent]
--- src/app/shared/services/prestation.service.ts
+++ src/app/shared/services/prestation.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+import { Category, Prestation } from "../models";
+
+export class PrestationGroupByCategory {
+  categoryId: string;
+  prestations: Prestation[]
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PrestationService {
+  constructor(private apollo: Apollo) {}
 
-  constructor() { }
+  /**
+   * Recover all prestations's category
+   */
+  getAllCategories(): Category[] {
+    return new Array<Category>();
+  }
+  /**
+   * Recover prestations category with at least one prestation
+   */
+  getCategoriesWithPrestations(): Category[] {
+    return new Array<Category>();
+  }
+  /**
+   * Recover all prestations
+   */
+  getPrestations(): Prestation[] {
+    return new Array<Prestation>();
+  }
+
+  getPrestationGroupByCategory(): PrestationGroupByCategory[] {
+    return new Array<PrestationGroupByCategory>();
+  }
 }
